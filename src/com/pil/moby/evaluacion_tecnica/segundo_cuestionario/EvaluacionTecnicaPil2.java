ac79d425ffda3c57b2712760e84cbec0948bca24
Matias Arias <matiasarias384@gmail.com>
2022-12-29 21:06:03 -0300

    #1 Create resolverPunto():void

--- src/com/pil/moby/evaluacion_tecnica/segundo_cuestionario/EvaluacionTecnicaPil2.java
+++ src/com/pil/moby/evaluacion_tecnica/segundo_cuestionario/EvaluacionTecnicaPil2.java
@@ -24,8 +24,8 @@ public class EvaluacionTecnicaPil2 {
         imprimirMensajePunto(1);
 
         // Desarrollo de la consigna 1.
-        inicializarCandidatos();
-        resolverPunto1();
+
+        resolverPunto1(inicializarCandidatos());
 
         imprimirMensajePunto(2);
 
@@ -49,8 +49,10 @@ public class EvaluacionTecnicaPil2 {
 
     }
 
-    private static void resolverPunto1() {
-
+    private static void resolverPunto1(List<Candidato> listaCandidatos) {
+        for (Candidato candidato: listaCandidatos) {
+            System.out.println(candidato);
+        }
     }
 
     private static void resolverPunto2() {
@@ -69,8 +71,75 @@ public class EvaluacionTecnicaPil2 {
         // TODO: Realizar implementación.
     }
 
-    private static void inicializarCandidatos() {
+    private static List<Candidato> inicializarCandidatos() {
+        List<Candidato> listaCandidatos = new ArrayList<>() ;
+        List<Tecnologia> listaTecnologias = new ArrayList<>();
+        Tecnologia java = new Tecnologia(1L , "Java");
+        Tecnologia angular = new Tecnologia(2L , "Angular");
+        Tecnologia sql = new Tecnologia(3L , "SQL");
+        Tecnologia nosql = new Tecnologia(4L , "NoSQL");
+        Tecnologia vue = new Tecnologia(5L , "Vue");
+        Tecnologia net = new Tecnologia(6L , ".NET");
+        Tecnologia mongo = new Tecnologia(7L , "MongoDB");
+        Tecnologia nodejs = new Tecnologia(8L , "NodeJS");
 
+        listaCandidatos.add(new Candidato(3L,"Jhon","Doe",2,5000d,new ArrayList<Tecnologia>(){
+            {
+                add(java);
+                add(angular);
+                add(sql);
+            }}));
+        listaCandidatos.add(new Candidato(7L,"Matias","Otamendi",9,3000d,new ArrayList<Tecnologia>(){
+            {
+                add(nodejs);
+                add(java);
+                add(sql);
+            }}));
+        listaCandidatos.add(new Candidato(1L,"Joaquin","Tagliafico",6,2000d,new ArrayList<Tecnologia>(){
+            {
+                add(mongo);
+                add(nosql);
+            }}));
+        listaCandidatos.add(new Candidato(4L,"Gaston","Mac Allister",1,5000.50d,new ArrayList<Tecnologia>(){
+            {
+                add(vue);
+                add(sql);
+                add(java);
+            }}));
+        listaCandidatos.add(new Candidato(5L,"Pablo","De Paul",4,9000.50d,new ArrayList<Tecnologia>(){{add(java);}}));
+        listaCandidatos.add(new Candidato(2L,"Lucas","Di Maria",7,6000d,new ArrayList<Tecnologia>(){
+            {
+                add(java);
+                add(angular);
+                add(sql);
+            }}));
+        listaCandidatos.add(new Candidato(10L,"Lionel","Messi",10,10000.10d,new ArrayList<Tecnologia>(){
+            {
+                add(sql);
+                add(vue);
+                add(java);
+            }}));
+        listaCandidatos.add(new Candidato(9L,"Julian","Álvarez",3,5000d,new ArrayList<Tecnologia>(){
+            {
+                add(nosql);
+                add(mongo);
+                add(sql);
+                add(java);
+            }}));
+        listaCandidatos.add(new Candidato(8L,"Lula","Martinez",2,1000d,new ArrayList<Tecnologia>(){
+            {
+                add(net);
+                add(java);
+                add(angular);
+            }}));
+        listaCandidatos.add(new Candidato(6L,"Agustin","Dybala",2,1000d,new ArrayList<Tecnologia>(){
+            {
+                add(net);
+                add(java);
+                add(angular);
+            }}));
+
+        return listaCandidatos;
     }
 
     /**
